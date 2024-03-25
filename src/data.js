83ce5089164717365f24d2a6009de532175be348
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-03-25 03:07:43 +0100

    slider component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 0,
+    img: 'https://i.ibb.co/LrvsDqx/freestocks-3-Q3ts-J01nc-unsplash.jpg',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+    bg: 'f5fafd',
+  },
+  {
+    id: 1,
+    img: 'https://i.ibb.co/sKj7kkg/jacek-dylag-jo8-C9bt3uo8-unsplash.jpg',
+    title: 'WINTER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/7N2rKzX/lucrezia-carnelos-w-Q9-Vu-P-Njr4-unsplash.jpg',
+    title: 'POPULAR SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS.",
+    bg: 'fbf0f4',
+  },
+];
