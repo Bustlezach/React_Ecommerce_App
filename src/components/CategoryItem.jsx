3e683284a3bcc323cad0618d375fb499485b25f0
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-03-25 11:50:29 +0100

    rest of category component

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Title = styled.h1``;
-const Info = styled.div``;
-const Button = styled.button``;
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 50vh;
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Title = styled.h1`
+  color: white;
+  margin-buttom: 20px;
+`;
+
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  background-color: white;
+  color: gray;
+  font-weight: 600;
+`;
 
 const CategoryItem = ({item}) => {
   return (
--- src/data.js
+++ src/data.js
@@ -25,17 +25,34 @@ export const sliderItems = [
 export const categories = [
   {
     id: 0,
-    img: 'https://i.ibb.co/LrvsDqx/freestocks-3-Q3ts-J01nc-unsplash.jpg',
-    title: 'SUMMER SALE',
+    img: 'https://i.ibb.co/gVgQHXf/the-nix-company-4-Hmj9gky-M6c-unsplash.jpg',
+    title: 'SHIRT STYLE!',
   },
   {
     id: 1,
-    img: 'https://i.ibb.co/sKj7kkg/jacek-dylag-jo8-C9bt3uo8-unsplash.jpg',
-    title: 'WINTER SALE',
+    img: 'https://i.ibb.co/XZDmNkB/tamanna-rumee-e-D1-RNYzz-Uxc-unsplash.jpg',
+    title: 'LOUNGEWEAR LOVE',
   },
   {
     id: 2,
-    img: 'https://i.ibb.co/7N2rKzX/lucrezia-carnelos-w-Q9-Vu-P-Njr4-unsplash.jpg',
-    title: 'POPULAR SALE',
+    img: 'https://i.ibb.co/G0twGXk/charlesdeluvio-FK81rxil-UXg-unsplash.jpg',
+    title: "MEN'S WEAR",
   },
 ];
+
+
+export const popularProducts = [
+  {
+    id: 0,
+    img: 'https://i.ibb.co/gVgQHXf/the-nix-company-4-Hmj9gky-M6c-unsplash.jpg',
+  },
+  {
+    id: 1,
+    img: 'https://i.ibb.co/XZDmNkB/tamanna-rumee-e-D1-RNYzz-Uxc-unsplash.jpg',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/G0twGXk/charlesdeluvio-FK81rxil-UXg-unsplash.jpg',
+  },
+];
+
